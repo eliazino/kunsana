5b9f2a3deec5778563bbf520ae9ee513d44236cf
Elias <elias.akin@gmail.com>
2016-12-29 08:57:36 +0100

    Revert "main-files commit"
    This reverts commit 59ec02182fc9d935f4da1ecea5159ec5806527ab.

--- destroy-2.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-setcookie("UID2","",time()-3,"/");
-setcookie("sk2","",time()-3,"/");
-echo "<script type='text/javascript'> 
-location.replace('artist-2'); 
-</script>";
-?>
--- destroy.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-setcookie("UID1","",time()-3,"/");
-setcookie("sk","",time()-3,"/");
-echo "<script type='text/javascript'> 
-location.replace('index'); 
-</script>";
-?>
